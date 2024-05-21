29c4ad9640f2264c5e43cc91b3a9ed27c63c9eea
Martin Orsag <martin.orsag@websupport.eu>
2024-05-21 18:11:34 +0200

    Homepage content.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,40 @@
+import ProductList from "@/components/ProductList"
+import CategoryList from "@/components/CategoryList"
+import Skeleton from "@/components/Skeleton"
+import Slider from "@/components/Slider"
+import { Suspense } from "react"
+
 const HomePage = () => {
   return (
-    <div className=''>HomePage</div>
+    <div className="">
+    <Slider />
+    <div className="mt-24 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64">
+      <h1 className="text-2xl">Featured Products</h1>
+      <Suspense fallback={<Skeleton />}>
+        <ProductList
+          categoryId={process.env.FEATURED_PRODUCTS_FEATURED_CATEGORY_ID!}
+          limit={4}
+        />
+      </Suspense>
+    </div>
+    <div className="mt-24">
+      <h1 className="text-2xl px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64 mb-12">
+        Categories
+      </h1>
+      <Suspense fallback={<Skeleton />}>
+        <CategoryList />
+      </Suspense>
+    </div>
+    <div className="mt-24 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64">
+      <h1 className="text-2xl">New Products</h1>
+      <Suspense fallback={<Skeleton />}>
+        <ProductList
+          categoryId={process.env.FEATURED_PRODUCTS_NEW_CATEGORY_ID!}
+          limit={4}
+        />
+      </Suspense>
+    </div>
+  </div>
   )
 }
 
